27ba429a35b7309d31c5cdb6375c9295eca9d65b
Cognozire <114404980+cognozire@users.noreply.github.com>
2023-08-12 16:24:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,20 +14,13 @@ st.title("Audio Processing App")
 uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav"])
 
 if uploaded_file is not None:
-    temp_audio_dir = "temp_audio"
-    os.makedirs(temp_audio_dir, exist_ok=True)
-    temp_audio_file_path = os.path.join(temp_audio_dir, uploaded_file.name)
-    
+    # Save the uploaded file to a temporary location
+    temp_audio_file_path = os.path.join("temp_audio", uploaded_file.name)
     with open(temp_audio_file_path, "wb") as f:
         f.write(uploaded_file.read())
 
-    # Convert the uploaded file to WAV format using librosa
-    
-    y, sr = librosa.load(temp_audio_file_path, sr=None)
-    temp_wav_file_path = os.path.splitext(temp_audio_file_path)[0] + ".wav"
-    librosa.output.write_wav(temp_wav_file_path, y, sr)
-    
-    samplerate = sr  # Use the sample rate from the converted WAV file
+    # Rest of your code remains the same
+    samplerate = 44100
     win_s = 4096
     hop_s = 512
     pitch_o = aubio.pitch("yin", win_s, hop_s, samplerate)
@@ -35,7 +28,7 @@ if uploaded_file is not None:
     pitch_o.set_tolerance(0.8)
 
     if st.button("Calculate Similarity"):
-        audio_file = aubio.source(temp_wav_file_path, samplerate, hop_s)
+        audio_file = aubio.source(temp_audio_file_path, samplerate, hop_s)
 
         notes = []
 
@@ -52,10 +45,12 @@ if uploaded_file is not None:
                 break
         audio_file.close()
         
-        # Delete the temporary audio files after processing
+        # Delete the temporary audio file after processing
         os.remove(temp_audio_file_path)
-        os.remove(temp_wav_file_path)
         
         note_counts = Counter(notes)
         most_common_note = note_counts.most_common(1)[0][0]
-        st.write(f"The most common note in the audio is: {most_common_note}")
+        if selected_note == most_common_note:
+            st.write(f"The given note is {selected_note}.")
+        else:
+            st.write(f"The given note doesn't match with {selected_note}.")
