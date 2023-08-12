1b88ff2be9286032f51b74386fba15bcc667afed
Cognozire <114404980+cognozire@users.noreply.github.com>
2023-08-12 14:15:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-import streamlit as st
-
-import random
-import numpy as np
-import aubio
-from collections import Counter
-
-import os
-def midi_to_note_name(midi_note):
-    note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-    note_name = note_names[midi_note % 12]
-    return note_name
-
-st.title("Audio Processing App")
-
-uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
-
-# Dropdown menu with musical notes
-musical_notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-selected_note = st.selectbox("Select a musical note", musical_notes)
-
 if uploaded_file is not None:
     samplerate = 44100
     win_s = 4096
@@ -26,16 +5,12 @@ if uploaded_file is not None:
     pitch_o = aubio.pitch("yin", win_s, hop_s, samplerate)
     pitch_o.set_unit("midi")
     pitch_o.set_tolerance(0.8)
-
+    audio_file = aubio.source(uploaded_file, samplerate, hop_s)
     if st.button("Calculate Similarity"):
-        audio_file = aubio.source(uploaded_file, samplerate, hop_s)
-
         notes = []
-
         while True:
             samples, read = audio_file()
             pitch = pitch_o(samples)[0]
-
             if pitch != 0:
                 note = int(pitch)
                 note_name = midi_to_note_name(note)
@@ -50,7 +25,3 @@ if uploaded_file is not None:
             st.write(f"The given note is {selected_note}.")
         else:
             st.write(f"The given not doesn't matches with {selected_note}.")
-
-
-
-
