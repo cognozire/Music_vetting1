ea5d8e5527cc28cecfb35de51e5f5fffa5ff2f4c
Cognozire <114404980+cognozire@users.noreply.github.com>
2023-08-12 18:26:41 +0530

    Update app.py
    final changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import os
+
+import numpy as np
 import aubio
 from collections import Counter
 
+import os
 def midi_to_note_name(midi_note):
     note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
     note_name = note_names[midi_note % 12]
@@ -12,14 +14,22 @@ st.title("Audio Processing App")
 
 uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav"])
 
+   
+
+# Dropdown menu with musical notes
+musical_notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+selected_note = st.selectbox("Select a musical note", musical_notes)
+
 if uploaded_file is not None:
-    # Save the uploaded file to a temporary location
-    temp_audio_file_path = os.path.join("temp_audio", uploaded_file.name)
+    temp_audio_dir = "temp_audio"
+    os.makedirs(temp_audio_dir, exist_ok=True)  # Create the temporary directory if it doesn't exist
+    temp_audio_file_path = os.path.join(temp_audio_dir, uploaded_file.name)
+    
     with open(temp_audio_file_path, "wb") as f:
         f.write(uploaded_file.read())
 
     # Rest of your code remains the same
-    samplerate = 44100
+    samplerate = 48000
     win_s = 4096
     hop_s = 512
     pitch_o = aubio.pitch("yin", win_s, hop_s, samplerate)
@@ -43,14 +53,18 @@ if uploaded_file is not None:
             if read < hop_s:
                 break
         audio_file.close()
-
+        
         # Delete the temporary audio file after processing
         os.remove(temp_audio_file_path)
-
+        
         note_counts = Counter(notes)
         most_common_note = note_counts.most_common(1)[0][0]
-        selected_note = "C"  # Define the selected note here
         if selected_note == most_common_note:
             st.write(f"The given note is {selected_note}.")
         else:
             st.write(f"The given note doesn't match with {selected_note}.")
+
+
+
+
+
