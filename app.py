14f0a80d3c5bbc4c9eb5589bc4fbd52ea06431a6
Cognozire <114404980+cognozire@users.noreply.github.com>
2023-08-12 16:24:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os
 import aubio
 from collections import Counter
-import librosa
+
 def midi_to_note_name(midi_note):
     note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
     note_name = note_names[midi_note % 12]
