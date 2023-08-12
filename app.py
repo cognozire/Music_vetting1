bfa7ad21a86c94cfe8c5031beb290387c49df2dc
Cognozire <114404980+cognozire@users.noreply.github.com>
2023-08-12 16:19:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-
-import numpy as np
+import os
 import aubio
 from collections import Counter
 
-import os
 def midi_to_note_name(midi_note):
     note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
     note_name = note_names[midi_note % 12]
@@ -14,22 +12,21 @@ st.title("Audio Processing App")
 
 uploaded_file = st.file_uploader("Choose an audio file", type=["mp3", "wav"])
 
-   
-
-# Dropdown menu with musical notes
-musical_notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-selected_note = st.selectbox("Select a musical note", musical_notes)
-
 if uploaded_file is not None:
     temp_audio_dir = "temp_audio"
-    os.makedirs(temp_audio_dir, exist_ok=True)  # Create the temporary directory if it doesn't exist
+    os.makedirs(temp_audio_dir, exist_ok=True)
     temp_audio_file_path = os.path.join(temp_audio_dir, uploaded_file.name)
     
     with open(temp_audio_file_path, "wb") as f:
         f.write(uploaded_file.read())
 
-    # Rest of your code remains the same
-    samplerate = 44100
+    # Convert the uploaded file to WAV format using librosa
+    import librosa
+    y, sr = librosa.load(temp_audio_file_path, sr=None)
+    temp_wav_file_path = os.path.splitext(temp_audio_file_path)[0] + ".wav"
+    librosa.output.write_wav(temp_wav_file_path, y, sr)
+    
+    samplerate = sr  # Use the sample rate from the converted WAV file
     win_s = 4096
     hop_s = 512
     pitch_o = aubio.pitch("yin", win_s, hop_s, samplerate)
@@ -37,7 +34,7 @@ if uploaded_file is not None:
     pitch_o.set_tolerance(0.8)
 
     if st.button("Calculate Similarity"):
-        audio_file = aubio.source(temp_audio_file_path, samplerate, hop_s)
+        audio_file = aubio.source(temp_wav_file_path, samplerate, hop_s)
 
         notes = []
 
@@ -54,17 +51,10 @@ if uploaded_file is not None:
                 break
         audio_file.close()
         
-        # Delete the temporary audio file after processing
+        # Delete the temporary audio files after processing
         os.remove(temp_audio_file_path)
+        os.remove(temp_wav_file_path)
         
         note_counts = Counter(notes)
         most_common_note = note_counts.most_common(1)[0][0]
-        if selected_note == most_common_note:
-            st.write(f"The given note is {selected_note}.")
-        else:
-            st.write(f"The given note doesn't match with {selected_note}.")
-
-
-
-
-
+        st.write(f"The most common note in the audio is: {most_common_note}")
